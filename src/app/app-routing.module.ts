7f3f260a010c3469f46de67733ba18ace1df19f7
Gabriel Pastrana <gapasva.10@gmail.com>
2020-05-25 03:03:38 -0600

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "src/app/components/home/home.component";
+import { LoginComponent } from "src/app/components/user/login/login.component";
+import { RegisterComponent } from "src/app/components/user/register/register.component";
+import { RegisterUserComponent } from "src/app/components/user/register-user/register-user.component";
+import { RegisterCompanyComponent } from "src/app/components/user/register-company/register-company.component";
+import { ProfileComponent } from "src/app/components/user/profile/profile.component";
+import { Page404Component } from "src/app/components/page404/page404.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "login", component: LoginComponent},
+  {path: "register", component: RegisterComponent},
+  {path: "register-user", component: RegisterUserComponent},
+  {path: "register-company", component: RegisterCompanyComponent},
+  {path: "profile", component: ProfileComponent},
+  {path: "**", component: Page404Component}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { RegisterUserComponent } from './components/user/register-user/register-user.component';
+import { RegisterCompanyComponent } from './components/user/register-company/register-company.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { Page404Component } from './components/page404/page404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    RegisterUserComponent,
+    RegisterCompanyComponent,
+    ProfileComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
